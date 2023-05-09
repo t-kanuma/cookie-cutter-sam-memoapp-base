e0f54b573b164be6c40088b61bbc2ad27dbea493
“Takahiro <takahiro-kanuma@gmogshd.com>
2023-05-09 17:39:53 +0900

    added sqs put.

--- {{cookiecutter.project_name}}/functions/createMemo/app.ts
+++ {{cookiecutter.project_name}}/functions/createMemo/app.ts
@@ -5,6 +5,7 @@ import {
   PutCommandOutput,
 } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { v4 as uuidv4 } from "uuid";
 import { InitMemo, Memo } from "./types";
@@ -57,8 +58,6 @@ export const lambdaHandler = async (
       new PutCommand(params)
     );
     console.log(`createResp: ${JSON.stringify(createResp)} `);
-
-    return createResponse(200, "created memo successfully.");
   } catch (err) {
     if (err instanceof Error) {
       throw new Error(
@@ -68,8 +67,28 @@ export const lambdaHandler = async (
       throw err;
     }
   }
-};
 
+  // Notify that the memo is created
+  const sqsClient: SQSClient = new SQSClient({ region: process.env.REGION });
+  const sqsParams = {
+    QueueUrl: process.env.QUEUE_URL,
+    MessageBody: JSON.stringify(newMemo),
+  };
+  try {
+    const sqsResp = await sqsClient.send(new SendMessageCommand(sqsParams));
+    console.log(`sqsResp: ${JSON.stringify(sqsResp)}`);
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(
+        `error on sending message to sqs: ${JSON.stringify(err.message)}`
+      );
+    } else {
+      throw err;
+    }
+  }
+
+  return createResponse(200, "created memo successfully.");
+};
 const isInitMemoType = (initMemo: InitMemo): initMemo is InitMemo => {
   const { title, text } = initMemo;
 
